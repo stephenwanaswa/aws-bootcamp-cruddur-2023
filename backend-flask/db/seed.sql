abf72ee68a64156f12af2ae40d6feaedcc0229ba
stephenwanaswa <stephenwanaswa@gmail.com>
2023-03-22 13:13:51 +0000

    seed update

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,7 +3,7 @@ INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
   ('Andrew Brown', 'andrewbrown', 'andrewbrown@exampro.co','MOCK'),
   ('Andrew Bayko', 'bayko', 'bayko@exampro.co','MOCK'),
-  ('Stephen Wanaswa', 'stephenwanaswa', 'stephenwanaswa@gmail.com','MOCK'),;
+  ('Londo Mollari', 'londo', 'lmollari@centari.com','MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
