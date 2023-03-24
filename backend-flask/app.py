67470e46b5f953aa1e1fe06efc299206f69e02e2
stephenwanaswa <stephenwanaswa@gmail.com>
2023-03-24 13:57:26 +0000

    minor update

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -252,7 +252,7 @@ def data_search():
 @app.route("/api/activities", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities():
-  user_handle  = 'stephenwanaswa'
+  user_handle  = 'andrewbrown'
   message = request.json['message']
   ttl = request.json['ttl']
   model = CreateActivity.run(message, user_handle, ttl)
--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -96,7 +96,13 @@ class Db:
     ) object_row);
     """
     return sql
-  
+  def query_wrap_array(self,template):
+    sql = f"""
+    (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
+    {template}
+    ) array_row);
+    """
+    return sql
   def print_sql_err(self,err):
     # get details about the exception
     err_type, err_obj, traceback = sys.exc_info()
