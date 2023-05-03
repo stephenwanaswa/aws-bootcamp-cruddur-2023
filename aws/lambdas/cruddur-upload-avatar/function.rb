e16a3278694d71c2f110f0ff9dde661381cf9988
stephenwanaswa <stephenwanaswa@gmail.com>
2023-05-03 14:17:09 +0000

    Avatar upload WIP

--- aws/lambdas/cruddur-upload-avatar/function.rb
+++ aws/lambdas/cruddur-upload-avatar/function.rb
@@ -10,39 +10,48 @@ def handler(event:, context:)
     { 
       headers: {
         "Access-Control-Allow-Headers": "*, Authorization",
-        "Access-Control-Allow-Origin": "https://stephenwana-awsbootcamp-ei8eza00n9g.ws-eu96.gitpod.io/",
+        "Access-Control-Allow-Origin": "https://3000-stephenwana-awsbootcamp-q54ft565xqk.ws-eu96b.gitpod.io",
         "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
       },
-      statusCode: 200
+      statusCode: 200,
     }
   else
-    token = event['headers']['authorization'].split(' ')[1]
-    puts({step: 'presignedurl', access_token: token}.to_json)
+    # Check if the event object contains the required keys
+    if event['headers'] && event['headers']['authorization']
+      token = event['headers']['authorization'].split(' ')[1]
+      puts({step: 'presignedurl', access_token: token}.to_json)
 
-    body_hash = JSON.parse(event["body"])
-    extension = body_hash["extension"]
+      body_hash = JSON.parse(event["body"])
+      extension = body_hash["extension"]
 
-    decoded_token = JWT.decode token, nil, false
-    cognito_user_uuid = decoded_token[0]['sub']
+      decoded_token = JWT.decode token, nil, false
+      cognito_user_uuid = decoded_token[0]['sub']
 
-    s3 = Aws::S3::Resource.new
-    bucket_name = ENV["UPLOADS_BUCKET_NAME"]
-    object_key = "#{cognito_user_uuid}.#{extension}"
+      s3 = Aws::S3::Resource.new
+      bucket_name = ENV["UPLOADS_BUCKET_NAME"]
+      object_key = "#{cognito_user_uuid}.#{extension}"
 
-    puts({object_key: object_key}.to_json)
+      puts({object_key: object_key}.to_json)
 
-    obj = s3.bucket(bucket_name).object(object_key)
-    url = obj.presigned_url(:put, expires_in: 60 * 5)
-    url # this is the data that will be returned
-    body = {url: url}.to_json
-    { 
-      headers: {
-        "Access-Control-Allow-Headers": "*, Authorization",
-        "Access-Control-Allow-Origin": "https://stephenwana-awsbootcamp-ei8eza00n9g.ws-eu96.gitpod.io/",
-        "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
-      },
-      statusCode: 200, 
-      body: body 
-    }
+      obj = s3.bucket(bucket_name).object(object_key)
+      url = obj.presigned_url(:put, expires_in: 60 * 5)
+      url # this is the data that will be returned
+      body = {url: url}.to_json
+      { 
+        headers: {
+          "Access-Control-Allow-Headers": "*, Authorization",
+          "Access-Control-Allow-Origin": "https://3000-stephenwana-awsbootcamp-q54ft565xqk.ws-eu96b.gitpod.io",
+          "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
+        },
+        statusCode: 200, 
+        body: body 
+      }
+    else
+      # Return an error response if the required keys are not present in the event object
+      {
+        statusCode: 400,
+        body: "Missing required headers"
+      }
+    end
   end # if 
 end # def handler
