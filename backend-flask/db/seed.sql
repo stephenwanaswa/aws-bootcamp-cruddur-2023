c0baba1e6f55aebed04b1480a0b06098dd9be401
stephenwanaswa <stephenwanaswa@gmail.com>
2023-06-14 12:13:08 +0000

    Fixed create activity hardcode

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,8 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
+  ('Steve Wekesa','steveiwans@gmail.com' , 'steveiwans' ,'MOCK'),
+  ('Papo Hapo','papohapo69@gmail.com' , 'papohapo' ,'MOCK'),
   ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
