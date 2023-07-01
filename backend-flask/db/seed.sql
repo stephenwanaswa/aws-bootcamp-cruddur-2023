7db7db84d0d32bb89a9f6bd03f8cd6ff4f9b36df
stephenwanaswa <stephenwanaswa@gmail.com>
2023-07-01 04:37:22 +0000

    Cleanup and time zone fix

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -8,7 +8,12 @@ VALUES
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'steveiwans' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
-  )
+  ),
+  (
+    (SELECT uuid from public.users WHERE users.handle = 'londo' LIMIT 1),
+    'Another seed data!',
+    current_timestamp + interval '10 day'
+  );
